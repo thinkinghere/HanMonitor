3b0ec51435f5d4124c3133b56a9e4575cad36313
hanzengxin <hanzengxin@dintech.com>
2020-12-24 15:17:36 +0800

    add trigger

--- monitor/admin.py
+++ monitor/admin.py
@@ -19,9 +19,29 @@ class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'interval', 'plugin_name')
 
 
+class TriggerExpressionInline(admin.TabularInline):
+    model = models.TriggerExpression
+    # exclude = ('memo',)
+    # readonly_fields = ['create_date']
+
+
+class TriggerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'severity', 'enabled')
+    inlines = [TriggerExpressionInline, ]  # 有外键关联关系的可以直接内嵌到相应的页面
+    # filter_horizontal = ('expressions',)
+
+
+class TriggerExpressionAdmin(admin.ModelAdmin):
+    list_display = (
+        'trigger', 'service', 'service_index', 'specified_index_key', 'operator_type', 'data_calc_func', 'threshold',
+        'logic_type')
+
+
 admin.site.register(models.Template, TemplateAdmin)
 admin.site.register(models.Host, HostAdmin)
 admin.site.register(models.Service, ServiceAdmin)
+admin.site.register(models.Trigger, TriggerAdmin)
+admin.site.register(models.TriggerExpression, TriggerExpressionAdmin)
 
 admin.site.register(models.HostGroup)
 admin.site.register(models.ServiceIndex)
